d3abbba076bb12c765ba73df05798665227af426
TahirSkill23 <tahirs9045@gmail.com>
2024-01-07 19:00:44 +0530

    First java file or program

---
+++ src/FirstJavaFile.java
@@ -0,0 +1,11 @@
+
+public class FirstJavaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First java file to commit on GitHub");
+
+	}
+
+}
